879d0ad2ce9668a0cc037c1e88f8debf14c1e2d8
Acasiax <117105267+Acasiax@users.noreply.github.com>
2024-02-19 20:55:22 +0900

    설정 화면을 구현했습니다.

--- SLAY_iOS/SLAY/SettingsView.swift
+++ SLAY_iOS/SLAY/SettingsView.swift
@@ -11,43 +11,48 @@ struct SettingsView: View {
     // 앱 버전을 저장할 변수
     let appVersion: String = Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String ?? "N/A"
     
+ 
+    
     var body: some View {
         
         
-        NavigationView {
-            List {
-                Section(header: Text("일반 설정")) {
-                    NavigationLink(destination: Text("일반 설정 화면")) {
+        NavigationStack {
+            ZStack{
+                
+                List {
+                    
+                    Section(header: Text("앱 정보")) {
                         HStack {
-                            Image(systemName: "gear")
-                            Text("일반")
+                            Image(systemName: "info.circle")
+                            Text("버전:")
+                            Spacer()
+                            Text("\(appVersion)")
                         }
                     }
+                    
+//                    Section(header: Text("업데이트")) {
+//                        Button(action: {
+//                            // 업데이트 확인 및 처리 코드
+//                            if let url = URL(string: "https://apps.apple.com/your-app-id") {
+//                                 UIApplication.shared.open(url, options: [:], completionHandler: nil)
+//                             }
+//                        }) {
+//                            HStack {
+//                                Image(systemName: "arrow.down.circle")
+//                                Text("업데이트 확인")
+//                            }
+//                        }
+//                    }
                 }
-                
-                Section(header: Text("앱 정보")) {
-                    HStack {
-                        Image(systemName: "info.circle")
-                        Text("버전: \(appVersion)")
-                    }
-                }
-                
-                Section(header: Text("업데이트")) {
-                    Button(action: {
-                        // 업데이트 확인 및 처리 코드
-                    }) {
-                        HStack {
-                            Image(systemName: "arrow.down.circle")
-                            Text("업데이트 확인")
-                        }
-                    }
-                }
+               // .background(.pink)
+                .background(LinearGradient(colors: [.green.opacity(0.3), .blue.opacity(0.5)],
+                                           startPoint: .topLeading, endPoint: .bottomTrailing)
+                )
+                .scrollContentBackground(.hidden)
+                .listStyle(InsetGroupedListStyle())
+                .navigationTitle("설정")
+               
             }
-            
-            .listStyle(InsetGroupedListStyle())
-            
-            .navigationTitle("설정")
-            
         }
     }
 }
@@ -55,5 +60,7 @@ struct SettingsView: View {
 struct SettingsView_Previews: PreviewProvider {
     static var previews: some View {
         SettingsView()
+        // .preferredColorScheme(.dark) // 미리보기를 다크 모드로 설정
     }
 }
+
