08478b515e47703a7e50a96d67779d15ae24eeb2
Acasiax <117105267+Acasiax@users.noreply.github.com>
2024-02-19 15:50:08 +0900

    Initial Commit

---
+++ SLAY_iOS/SLAY_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  SLAY_iOSApp.swift
+//  SLAY_iOS
+//
+//  Created by 이윤지 on 2/19/24.
+//
+
+import SwiftUI
+
+@main
+struct SLAY_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
